19c77a055fb08966d6c18d9bc3125e0b9995bf90
RK Rukshan <44175424+RkRukshan@users.noreply.github.com>
2023-11-07 21:01:07 +0530

    create home.blade.php in cd auth

--- .history/resources/views/user/home.blade_20231107205943.php
+++ .history/resources/views/user/home.blade_20231107205943.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in as User!') }}
+                    {{ __('You are logged in as !') }}
                 </div>
             </div>
         </div>
