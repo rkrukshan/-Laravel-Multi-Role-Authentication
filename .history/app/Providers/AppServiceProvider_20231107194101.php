f5d330a401e7daf1973a6e327a6faf79798d3884
RK Rukshan <44175424+RkRukshan@users.noreply.github.com>
2023-11-07 19:43:46 +0530

    npm run dev & create user migration with model & php artisan migrate

--- .history/app/Providers/AppServiceProvider_20231107194101.php
+++ .history/app/Providers/AppServiceProvider_20231107194101.php
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
+        Schema::
     }
 }
--- .history/database/migrations/2014_10_12_000000_create_users_table_20231107193924.php
+++ .history/database/migrations/2014_10_12_000000_create_users_table_20231107193924.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->tinyInteger('is_admin')->default(0);
+            $table->tinyInteger('is_admin')
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
