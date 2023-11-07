a39e7b3a074aa842a50c2ea6e7b2ec72bd43c39e
RK Rukshan <44175424+RkRukshan@users.noreply.github.com>
2023-11-07 21:26:14 +0530

    change use class of HomeController becase of moving to cd auth

--- .history/app/Http/Controllers/Auth/HomeController_20231107211021.php
+++ .history/app/Http/Controllers/Auth/HomeController_20231107211021.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\user;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
--- .history/app/Http/Controllers/Auth/HomeController_20231107212412.php
+++ .history/app/Http/Controllers/Auth/HomeController_20231107212412.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
